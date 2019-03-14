f453303f0d4773df85909adf8296ca282930c56f
Matt Keeler <mjkeeler7@gmail.com>
2019-03-14 09:11:58 -0400

    Finish up the basic tests

--- bexpr/grammar_test.go
+++ bexpr/grammar_test.go
@@ -70,6 +70,149 @@ func TestFilterLangParsing(t *testing.T) {
 			},
 			err: nil,
 		},
+		"Double Quoted Value (Equal)": {
+			input:    "foo == \"bar\"",
+			expected: &MatchExpr{Selector: Selector{"foo"}, Operator: MatchEqual, Value: &Value{Raw: "bar"}},
+			err:      nil,
+		},
+		"Double Quoted Value (Not Equal)": {
+			input:    "foo != \"bar\"",
+			expected: &MatchExpr{Selector: Selector{"foo"}, Operator: MatchNotEqual, Value: &Value{Raw: "bar"}},
+			err:      nil,
+		},
+		"Double Quoted Value (In)": {
+			input:    "\"foo\" in bar",
+			expected: &MatchExpr{Selector: Selector{"bar"}, Operator: MatchIn, Value: &Value{Raw: "foo"}},
+			err:      nil,
+		},
+		"Double Quoted Value (Not In)": {
+			input:    "\"foo\" not in bar",
+			expected: &MatchExpr{Selector: Selector{"bar"}, Operator: MatchNotIn, Value: &Value{Raw: "foo"}},
+			err:      nil,
+		},
+		"Backtick Quoted Value (Equal)": {
+			input:    "foo == `bar`",
+			expected: &MatchExpr{Selector: Selector{"foo"}, Operator: MatchEqual, Value: &Value{Raw: "bar"}},
+			err:      nil,
+		},
+		"Backtick Quoted Value (Not Equal)": {
+			input:    "foo != `bar`",
+			expected: &MatchExpr{Selector: Selector{"foo"}, Operator: MatchNotEqual, Value: &Value{Raw: "bar"}},
+			err:      nil,
+		},
+		"Backtick Quoted Value (In)": {
+			input:    "`foo` in bar",
+			expected: &MatchExpr{Selector: Selector{"bar"}, Operator: MatchIn, Value: &Value{Raw: "foo"}},
+			err:      nil,
+		},
+		"Backtick Quoted Value (Not In)": {
+			input:    "`foo` not in bar",
+			expected: &MatchExpr{Selector: Selector{"bar"}, Operator: MatchNotIn, Value: &Value{Raw: "foo"}},
+			err:      nil,
+		},
+		// This is standard boolean expression precedence
+		//
+		// 1 - not
+		// 2 - and
+		// 3 - or
+		"Logical Operator Precedence": {
+			input: "x in foo and not str == something or list is empty",
+			expected: &BinaryExpr{
+				Operator: BinaryOpOr,
+				Left: &BinaryExpr{
+					Operator: BinaryOpAnd,
+					Left:     &MatchExpr{Selector: Selector{"foo"}, Operator: MatchIn, Value: &Value{Raw: "x"}},
+					Right: &UnaryExpr{
+						Operator: UnaryOpNot,
+						Operand:  &MatchExpr{Selector: Selector{"str"}, Operator: MatchEqual, Value: &Value{Raw: "something"}},
+					},
+				},
+				Right: &MatchExpr{Selector: Selector{"list"}, Operator: MatchIsEmpty, Value: nil},
+			},
+			err: nil,
+		},
+		// not in the absence of parentheses would normally get applied
+		// to a MatchExpr
+		//
+		// or operators normal are the last to be applied but here they
+		// happen earlier
+		"Logical Operator Precedence (Parenthesized)": {
+			input: "x in foo and not (str == something or list is empty)",
+			expected: &BinaryExpr{
+				Operator: BinaryOpAnd,
+				Left:     &MatchExpr{Selector: Selector{"foo"}, Operator: MatchIn, Value: &Value{Raw: "x"}},
+				Right: &UnaryExpr{
+					Operator: UnaryOpNot,
+					Operand: &BinaryExpr{
+						Operator: BinaryOpOr,
+						Left:     &MatchExpr{Selector: Selector{"str"}, Operator: MatchEqual, Value: &Value{Raw: "something"}},
+						Right:    &MatchExpr{Selector: Selector{"list"}, Operator: MatchIsEmpty, Value: nil},
+					},
+				},
+			},
+			err: nil,
+		},
+		"Extra Whitespace (Equal)": {
+			input:    "\t\r\n  foo \t\r\n == \t\r\n x \t\r\n",
+			expected: &MatchExpr{Selector: Selector{"foo"}, Operator: MatchEqual, Value: &Value{Raw: "x"}},
+			err:      nil,
+		},
+		"Extra Whitespace (Not Equal)": {
+			input:    "\t\r\n  foo \t\r\n != \t\r\n x \t\r\n",
+			expected: &MatchExpr{Selector: Selector{"foo"}, Operator: MatchNotEqual, Value: &Value{Raw: "x"}},
+			err:      nil,
+		},
+		"Extra Whitespace (In)": {
+			input:    "\t\r\n  foo \t\r\n in \t\r\n x \t\r\n",
+			expected: &MatchExpr{Selector: Selector{"x"}, Operator: MatchIn, Value: &Value{Raw: "foo"}},
+			err:      nil,
+		},
+		"Extra Whitespace (Not In)": {
+			input:    "\t\r\n  foo \t\r\n not \t\r\n in \t\r\n x \t\r\n",
+			expected: &MatchExpr{Selector: Selector{"x"}, Operator: MatchNotIn, Value: &Value{Raw: "foo"}},
+			err:      nil,
+		},
+		"Extra Whitespace (Is Empty)": {
+			input:    "\t\r\n  foo \t\r\n is \t\r\n empty \t\r\n",
+			expected: &MatchExpr{Selector: Selector{"foo"}, Operator: MatchIsEmpty, Value: nil},
+			err:      nil,
+		},
+		"Extra Whitespace (Is Not Empty)": {
+			input:    "\t\r\n  foo \t\r\n is \t\r\n not \t\r\n empty \t\r\n",
+			expected: &MatchExpr{Selector: Selector{"foo"}, Operator: MatchIsNotEmpty, Value: nil},
+			err:      nil,
+		},
+		"Extra Whitespace (Not)": {
+			input: "\t\r\n not \t\r\n  foo \t\r\n in \t\r\n x \t\r\n",
+			expected: &UnaryExpr{
+				Operator: UnaryOpNot,
+				Operand:  &MatchExpr{Selector: Selector{"x"}, Operator: MatchIn, Value: &Value{Raw: "foo"}},
+			},
+			err: nil,
+		},
+		"Extra Whitespace (And)": {
+			input: "\t\r\n foo \t\r\n == \t\r\n x \t\r\n and \t\r\n y \t\r\n is \t\r\n empty \t\r\n",
+			expected: &BinaryExpr{
+				Operator: BinaryOpAnd,
+				Left:     &MatchExpr{Selector: Selector{"foo"}, Operator: MatchEqual, Value: &Value{Raw: "x"}},
+				Right:    &MatchExpr{Selector: Selector{"y"}, Operator: MatchIsEmpty, Value: nil},
+			},
+			err: nil,
+		},
+		"Extra Whitespace (Or)": {
+			input: "\t\r\n foo \t\r\n == \t\r\n x \t\r\n or \t\r\n y \t\r\n is \t\r\n empty \t\r\n",
+			expected: &BinaryExpr{
+				Operator: BinaryOpOr,
+				Left:     &MatchExpr{Selector: Selector{"foo"}, Operator: MatchEqual, Value: &Value{Raw: "x"}},
+				Right:    &MatchExpr{Selector: Selector{"y"}, Operator: MatchIsEmpty, Value: nil},
+			},
+			err: nil,
+		},
+		"Extra Whitespace (Parentheses)": {
+			input:    "\t\r\n ( \t\r\n foo \t\r\n == \t\r\n x \t\r\n ) \t\r\n",
+			expected: &MatchExpr{Selector: Selector{"foo"}, Operator: MatchEqual, Value: &Value{Raw: "x"}},
+			err:      nil,
+		},
 	}
 
 	for name, tcase := range tests {
